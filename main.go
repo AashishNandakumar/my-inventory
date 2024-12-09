7f1e5b4479ad3a5b24fdf24a98b4c4286be5c1fe
aashishnandakumar <aashishnkumar@gmail.com>
2024-12-09 13:58:50 +0530

    updated port no

--- main.go
+++ main.go
@@ -2,6 +2,6 @@ package main
 
 func main() {
 	app := App{}
-	app.Initialize("localhost", "5436", "admin", "admin", "inventory")
+	app.Initialize("localhost", "5432", "admin", "admin", "inventory")
 	app.Run("localhost:10000")
 }
